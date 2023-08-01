679084bd761cc7aec13320d314f82f2ae172e831
Harshil Rama <harshilr98@gmail.com>
2023-07-31 19:24:07 -0500

    Worked on activities api

--- api/activities.js
+++ api/activities.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const activitiesRouter = express.Router();
 const {
   getAllActivities,
   getPublicRoutinesByActivity,
@@ -9,12 +9,77 @@ const {
   getActivityByName,
 } = require("../db");
 
+const { requireUser } = require("./utils");
+
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
 
+activitiesRouter.get("/", async (req, res, next) => {
+  try {
+    const activities = await getAllActivities();
+    res.send(activities);
+  } catch (error) {
+    next(error);
+  }
+})
+
 // POST /api/activities
+
+activitiesRouter.post("/", async (req, res, next) => {
+  try {
+    const { name, description } = req.body;
+    const activity = await getActivityByName(name);
+    if (activity) {
+      next({
+        error: "Error",
+        message: `An activity with name ${name} already exists`,
+        name: "Error",
+      });
+    } else {
+      const newActivity = await createActivity({ name, description })
+      res.send(newActivity);
+    }
+  } catch (error) {
+    next(error);
+  }
+})
 
 // PATCH /api/activities/:activityId
 
-module.exports = router;
+activitiesRouter.patch("/:activityId", requireUser, async (req, res, next) => {
+  const { activityId } = req.params;
+  const { name, description } = req.body;
+  const activity = await getActivityById(activityId);
+  try {
+    if (!activity) {
+      next({
+        error: "Error",
+        message: `Activity ${activityId} not found`,
+        name: "Error",
+      });
+    }
+
+    const existingActivity = await getActivityByName(name);
+    if (existingActivity && existingActivity.id !== activityId) {
+      next({
+        error: "Error",
+        message: `An activity with name ${name} already exists`,
+        name: "Error",
+      });
+    }
+
+    const updatedActivity = await updateActivity({
+      id: activityId,
+      name,
+      description,
+    });
+
+    res.send(updatedActivity);
+  } catch (error) {
+    next(error);
+  }
+
+});
+
+module.exports = activitiesRouter;
